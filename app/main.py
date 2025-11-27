fd3998732608aad80276f0c3a9d86e79046e2478
yashdodwani <yashdodwani64@gmail.com>
2025-11-27 09:45:45 +0530

    added local host

--- app/main.py
+++ app/main.py
@@ -24,6 +24,7 @@ _default_origins = [
     "http://localhost:3000",
     "http://localhost:5173",
     "http://localhost:8082",
+    "http://localhost:8080",
     # Lovable preview URL
     "https://id-preview--3a7b2998-fc47-4b75-9b46-fbfbfd416a18.lovable.app",
 
